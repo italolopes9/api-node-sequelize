9d09da7db6380eb43120624789549be71e9277c8
italo lopes <it-alo19@hotmail.com>
2020-07-18 16:22:54 -0300

    Finalizado relacionamento Services - Category

--- src/routes.js
+++ src/routes.js
@@ -13,12 +13,16 @@ const routes = express.Router();
 
 routes.post('/login', AuthController.login);
 
+//USER ROUTES
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
 
-routes.get('/users/:user_id/services', ServiceController.index);
-routes.post('/users/:user_id/services', ServiceController.store);
+//SERVICES ROUTES
+routes.get('/users/:user_id/services', ServiceController.listServiceForUser); //LISTA SERVICES POR USER
+routes.get('/categories/:category_id/services', ServiceController.listServiceForUser);//LISTA SERVICES POR CATEGORY
+routes.post('/users/:user_id/services', ServiceController.store); // CRIA SERVICE
 
+//CATEGORY ROUTES
 routes.get('/categories', CategoryController.index);
 routes.post('/categories', CategoryController.store);
 
